11907b5ffaaa7b0f498dd504a3d4a4ad4a7299fd
pkungu <pkungu@spsu.edu>
2012-11-19 22:03:29 -0500

    Update BookstoreComponent/bookstore.sql

--- BookstoreComponent/bookstore.sql
+++ BookstoreComponent/bookstore.sql
@@ -57,11 +57,11 @@ INSERT INTO `books` (`id`, `isbn`, `title`, `publisher`, `authors`, `count`, `pr
 INSERT INTO `books` (`id`, `isbn`, `title`, `publisher`, `authors`, `count`, `price`) VALUES
 (7, '9780123704900', 'Computer Architecture', 'Morgan Kaufmann', 'John L.Hennessy', 6, 64.99);
 INSERT INTO `books` (`id`, `isbn`, `title`, `publisher`, `authors`, `count`, `price`) VALUES
-(8, '9780201633610', 'Design Patterns', 'Addison Wesley, 'John Helm', 8, 65.99);
+(8, '9780201633610', 'Design Patterns', 'Addison Wesley', 'John Helm', 8, 65.99);
 INSERT INTO `books` (`id`, `isbn`, `title`, `publisher`, `authors`, `count`, `price`) VALUES
-(9, '9780321356680', 'Effective Java', 'Addison Wesley, 'Joshua Bloch, 10, 78.99);
+(9, '9780321356680', 'Effective Java', 'Addison Wesley', 'Joshua Bloch', 10, 78.99);
 INSERT INTO `books` (`id`, `isbn`, `title`, `publisher`, `authors`, `count`, `price`) VALUES
-(10, '9780321356665', 'Component Development', 'Pierce, 'Kai Qian, 10, 99.99);
+(10, '9780321356665', 'Component Development', 'Pierce', 'Kai Qian', 10, 99.99);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
